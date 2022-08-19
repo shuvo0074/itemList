fd5ca9b12a7480a0b9886f24878b598c594912b4
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-08-19 21:09:51 +0600

    add new user asset

--- app/assets/index.js
+++ app/assets/index.js
@@ -4,7 +4,11 @@ const login = {
   loginBg: require('./login/loginBg.png'),
   errorBg: require('./login/errorBg.png'),
 };
+const home = {
+  addUser: require('./home/addUser.png'),
+};
 
 export const assets = {
   login,
+  home
 };
