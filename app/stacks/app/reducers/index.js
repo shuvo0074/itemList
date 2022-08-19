580564ce70f4a54596b15f12ff2461114bc50d1d
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-08-19 20:10:24 +0600

    add dummy items in product list

--- app/stacks/app/reducers/index.js
+++ app/stacks/app/reducers/index.js
@@ -1,7 +1,32 @@
 import {TYPES} from '../../../const/types';
 
 const INITIAL_STATE = {
-  producList: [],
+  producList: [
+    {
+      title: 'Matcha Tea',
+      quantity: 0,
+      description: '',
+      category: 0,
+    },
+    {
+      title: 'Yogurt',
+      quantity: 10,
+      description: '',
+      category: 1,
+    },
+    {
+      title: 'Apples',
+      quantity: 4,
+      description: 'Green ones only',
+      category: 2,
+    },
+    {
+      title: 'mangoes',
+      quantity: 9,
+      description: 'Not important',
+      category: 2,
+    }
+  ],
 };
 
 export const PRODUCT_OBJECT = {
