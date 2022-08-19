57d06ed0af62b9c93306131d8ca617aa9f958774
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-08-19 18:14:42 +0600

    add login assets

---
+++ app/assets/index.js
@@ -0,0 +1,10 @@
+
+const login = {
+  icCorrect: require('./login/icCorrect.png'),
+  loginBg: require('./login/loginBg.png'),
+  errorBg: require('./login/errorBg.png'),
+};
+
+export const assets = {
+  login,
+};
