6c5328da14495e6d0c79a75c1d5f922710a9a530
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-08-19 20:16:22 +0600

    show product list in homepage

--- app/stacks/app/screens/Home.js
+++ app/stacks/app/screens/Home.js
@@ -9,104 +9,48 @@
 import React from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Dimensions,
+  SafeAreaView,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {useDispatch} from 'react-redux';
+import ItemListHeader from '../../../components/home/ItemListHeader';
+import ListItem from '../../../components/home/ListItem';
+import GlobalStyles from '../../../style';
+import {CATEGORY_LIST} from '../reducers';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const {height} = Dimensions.get('screen');
 
-const Home: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Result: () => Node = ({navigation}) => {
+  const dispatch = useDispatch();
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: GlobalStyles.COLOR_HOME,
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'dark-content'} />
+      <ItemListHeader username={'Shuvo'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        style={backgroundStyle}
+        contentContainerStyle={styles.scrollContainer}>
+        {CATEGORY_LIST.map((category, index) => (
+          <ListItem item={category} index={index} key={index} />
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  scrollContainer: {
+    minHeight: height,
   },
 });
 
-export default Home;
+export default Result;
